75959163d0467e3ad8302a3f400fd84716e1a5f4
Filipe Eder Rufino Carvalho <g00439340@atu.ie>
2024-12-01 22:40:18 +0000

    Updated permissions

--- weather.sh
+++ weather.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+echo "Downloading weather data"
 wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
+echo "Weather data downloaded"
